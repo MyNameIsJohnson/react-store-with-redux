56c0d78711b7a06a1b416adc38c7554b816a10c8
johnson <johnsonle2929@gmail.com>
2022-11-20 17:59:51 -0600

    Added action creator thunk

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { uiActions } from "./store/ui-slice";
-
+// import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 let isInitial = true;
 
 function App() {
@@ -18,50 +18,58 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
   console.log(notification);
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(
-        "https://react-redux-store-project-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-      // const responseData = await response.json();
-      // ^^^ don't care about the response data in this case, because for sending the card data I'm not interested in any response. So I don't even need to get to the response data. Instead, knowing that I don't have an error is enough.
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sending cart data successfully",
-        })
-      );
-    };
-
+    // Two ways to handle side effect, async tasks and redux
+    // 1. Inside the components (e.g. useEffect())
+    // const sendCartData = async () => {
+    // dispatch(
+    //   uiActions.showNotification({
+    //     status: "pending",
+    //     title: "Sending...",
+    //     message: "Sending cart data",
+    //   })
+    // );
+    // const response = await fetch(
+    //   "https://react-redux-store-project-default-rtdb.firebaseio.com/cart.json",
+    //   {
+    //     method: "PUT",
+    //     body: JSON.stringify(cart),
+    //   }
+    // );
+    // if (!response.ok) {
+    //   throw new Error("Sending cart data failed.");
+    // }
+    // const responseData = await response.json();
+    // ^^^ don't care about the response data in this case, because for sending the card data I'm not interested in any response. So I don't even need to get to the response data. Instead, knowing that I don't have an error is enough.
+    // dispatch(
+    //   uiActions.showNotification({
+    //     status: "success",
+    //     title: "Success!",
+    //     message: "Sending cart data successfully",
+    //   })
+    // );
+    // };
+    // if (isInitial) {
+    //   isInitial = false;
+    //   console.log(isInitial);
+    //   return;
+    // }
+    // sendCartData().catch((error) => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "error",
+    //       title: "Error!",
+    //       message: "Sending cart data failed",
+    //     })
+    //   );
+    // });
+    // 2. Inside the action creators (e.g. sendCartData from store/cart-slice)
     if (isInitial) {
       isInitial = false;
+      console.log(isInitial);
+
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <Fragment>
