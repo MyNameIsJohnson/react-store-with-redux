56b56f9b4f9fba0df4f486910ff2ac03f31c82e6
johnson <johnsonle2929@gmail.com>
2022-11-22 10:54:50 -0600

    Added fetching and sending cart data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 // import { uiActions } from "./store/ui-slice";
-import { sendCartData } from "./store/cart-slice";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 let isInitial = true;
 
 function App() {
@@ -17,60 +17,69 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
   console.log(notification);
+
+  // Two ways to handle side effect, async tasks and redux
+  // 1. Inside the components (e.g. useEffect())
+  // const sendCartData = async () => {
+  // dispatch(
+  //   uiActions.showNotification({
+  //     status: "pending",
+  //     title: "Sending...",
+  //     message: "Sending cart data",
+  //   })
+  // );
+  // const response = await fetch(
+  //   "https://react-redux-store-project-default-rtdb.firebaseio.com/cart.json",
+  //   {
+  //     method: "PUT",
+  //     body: JSON.stringify(cart),
+  //   }
+  // );
+  // if (!response.ok) {
+  //   throw new Error("Sending cart data failed.");
+  // }
+  // const responseData = await response.json();
+  // ^^^ don't care about the response data in this case, because for sending the card data I'm not interested in any response. So I don't even need to get to the response data. Instead, knowing that I don't have an error is enough.
+  // dispatch(
+  //   uiActions.showNotification({
+  //     status: "success",
+  //     title: "Success!",
+  //     message: "Sending cart data successfully",
+  //   })
+  // );
+  // };
+  // if (isInitial) {
+  //   isInitial = false;
+  //   console.log(isInitial);
+  //   return;
+  // }
+  // sendCartData().catch((error) => {
+  //   dispatch(
+  //     uiActions.showNotification({
+  //       status: "error",
+  //       title: "Error!",
+  //       message: "Sending cart data failed",
+  //     })
+  //   );
+  // });
+  // 2. Inside the action creators (e.g. sendCartData from store/cart-slice)
   useEffect(() => {
-    // Two ways to handle side effect, async tasks and redux
-    // 1. Inside the components (e.g. useEffect())
-    // const sendCartData = async () => {
-    // dispatch(
-    //   uiActions.showNotification({
-    //     status: "pending",
-    //     title: "Sending...",
-    //     message: "Sending cart data",
-    //   })
-    // );
-    // const response = await fetch(
-    //   "https://react-redux-store-project-default-rtdb.firebaseio.com/cart.json",
-    //   {
-    //     method: "PUT",
-    //     body: JSON.stringify(cart),
-    //   }
-    // );
-    // if (!response.ok) {
-    //   throw new Error("Sending cart data failed.");
-    // }
-    // const responseData = await response.json();
-    // ^^^ don't care about the response data in this case, because for sending the card data I'm not interested in any response. So I don't even need to get to the response data. Instead, knowing that I don't have an error is enough.
-    // dispatch(
-    //   uiActions.showNotification({
-    //     status: "success",
-    //     title: "Success!",
-    //     message: "Sending cart data successfully",
-    //   })
-    // );
-    // };
-    // if (isInitial) {
-    //   isInitial = false;
-    //   console.log(isInitial);
-    //   return;
-    // }
-    // sendCartData().catch((error) => {
-    //   dispatch(
-    //     uiActions.showNotification({
-    //       status: "error",
-    //       title: "Error!",
-    //       message: "Sending cart data failed",
-    //     })
-    //   );
-    // });
-    // 2. Inside the action creators (e.g. sendCartData from store/cart-slice)
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       console.log(isInitial);
 
       return;
     }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
     dispatch(sendCartData(cart));
   }, [cart, dispatch]);
+
   return (
     <Fragment>
       {notification && (
