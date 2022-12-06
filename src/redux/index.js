c0156084fec12a346ff263d6ec50c6f91f5bb6ea
johnson <johnsonle2929@gmail.com>
2022-12-05 18:36:45 -0600

    Updated redux index.js

--- src/redux/index.js
+++ src/redux/index.js
@@ -4,17 +4,10 @@ import productSlice from "./slices/product-slice";
 
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./slices/auth-slice";
-// import productReducer from "./slice/product-slice";
-import filterReducer from "./slices/filter-slice";
-// import cartReducer from "./slice/cart-slice";
-import checkoutReducer from "./slices/checkout-slice";
-import orderReducer from "./slices/order-slice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  filter: filterReducer,
-  checkout: checkoutReducer,
-  orders: orderReducer,
+
   ui: uiSlice.reducer,
   cart: cartSlice.reducer,
   product: productSlice.reducer,
